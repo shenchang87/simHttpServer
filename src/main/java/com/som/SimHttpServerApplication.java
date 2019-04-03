49f25e22d40d465a726ea6aba5172005ea267b1c
shenchang87 <846305275@qq.com>
2019-04-03 09:59:22 +0800

    new git

---
+++ src/main/java/com/som/SimHttpServerApplication.java
@@ -0,0 +1,14 @@
+package com.som;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimHttpServerApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SimHttpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/som/SimHttpServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.som;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimHttpServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
