9020ec0644882a1fb07b182fcdfd3d51688cbbbb
shenchang87 <846305275@qq.com>
2019-04-03 10:01:29 +0800

    v0.1

--- src/main/java/com/som/SimHttpServerApplication.java
+++ src/main/java/com/som/SimHttpServerApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimHttpServerApplication {
 
 	public static void main(String[] args) {
-		
+
+		String version="v.0.1";
 		SpringApplication.run(SimHttpServerApplication.class, args);
 	}
 
