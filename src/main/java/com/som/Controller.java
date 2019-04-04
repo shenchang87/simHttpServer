1a4ef1226954223d2237f26ce75c7b774cd96533
shenchang87 <846305275@qq.com>
2019-04-04 10:47:59 +0800

    abc

--- src/main/java/com/som/Controller.java
+++ src/main/java/com/som/Controller.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-	@RequestMapping("/api/serach22")
+	@RequestMapping("/api/serach git")
      public String     saveObj() {
          String msg= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root><msg>交易成功</msg><code>000</code><amt>22.24</amt><details><detail><fee>12.2</fee>    n    <date>2019-01</date></detail><detail><fee>12.3</fee><date>2019-02</date></detail></details></root>";
-         System.out.println("返回数据      "+msg);
+         System.out.println("返回数据"+msg);
          return   msg;
      }
-	 
+
 	 @RequestMapping("/api/serach1")
      public String saveObj1() {
-		 
+
 		 String msg= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root><msg>交易失败</msg><code>022</code></root>";
-		 
+
 		// String msg= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root><msg>交易成功</msg><code>000</code><amt>22.24</amt><details><detail><fee>12.2</fee><date>2019-01</date></detail><detail><fee>12.3</fee><date>2019-02</date></detail></details></root>";
-         System.out.println("返回数据        "+msg);
+         System.out.println("返回数据"+msg);
          return   msg;
 		 
 		 
